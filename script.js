ad14843aa3db91a40ddb72de0631ccb35685512e
Gabrielidelovski <164918814+Gabrielidelovski@users.noreply.github.com>
2025-04-02 14:11:10 -0300

    atualizar

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMcontentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
     let tamanhoAtualFonte= 1;
     const aumentaFonteBotao= document.getElementById('aumentar-fonte');
-
-    aumentaFonteBotao.addEventListener('click', fuction(){
-        tamanhoAtualFonte +=0.1;
-        document.body.style.fonteSize= '${tamanhoAtualFonte}rem';
+    
+    aumentaFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize= `${tamanhoAtualFonte}rem`;
     })
-    const diminuiFonteBotao= document.getElementById('diminuir-fonte')
-    diminuiFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize= '${tamanhoAtualFonte}rem';
+    const diminuirFonteBotao= document.getElementById('diminuir-fonte');
+    diminuirFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize= `${tamanhoAtualFonte}rem`;
     })
-})
+    })
